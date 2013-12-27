0bacdc8f872c52a5ba6d49d04cc96724833715ff
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-12-27 19:49:35 +1100

    Starting on js for dynamic properties

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(function() {
   // too lazy, just using jquery sorry
+
+  // get width and height of "markhepburn", plug those in so CSS can
+  // do the rest:
+  var $me    = $('#me'),
+      width  = $me.outerWidth(),
+      height = $me.outerHeight();
+  $me.css('margin-left', -width / 2);
+
+  // set up hovers
 });
